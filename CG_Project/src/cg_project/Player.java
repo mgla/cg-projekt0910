c39739980ea17a42fc68faf748bd9f4da1f8a5a7
christian.dernehl <christian.dernehl@web.de>
2010-01-21 13:54:13 +0000

    added necessary player file

---
+++ CG_Project/src/cg_project/Player.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cg_project;
+
+/**
+ *
+ * @author christiandernehl
+ */
+public class Player {
+    
+    private int points = 0;
+    
+    
+    
+    public Player(){
+        
+    }
+    
+    
+    void addPoints(int points){
+        this.points += points;
+    }
+}
